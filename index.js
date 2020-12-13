e6bce08d29b1d871fea3c1ff2cd5bbf5f62d4589
Conrad Buck <conartist6@gmail.com>
2020-12-12 20:16:02 -0500

    Better handling of exports

--- index.js
+++ index.js
@@ -6,8 +6,31 @@ const pkgNameExp = /(^@[^/]+\/[^/]+|[^./][^/]*)(.*)/;
 const { packageJson: pkg, path: pkgPath } = readPkgUp({ normalize: false });
 const root = dirname(pkgPath);
 
+function getImportPathFromPkg(pkg, isRequire) {
+  if (exports) {
+    const { exports } = pkg;
+    if (exports['.']) {
+      const dotExport = exports['.'];
+      if (typeof dotExport === 'object') {
+        const condition = isRequire ? 'require' : 'import';
+        if (dotExport[condition]) {
+          return dotExport[condition];
+        } else {
+          throw new Error(`${condition} condition not found in exports['.']`);
+        }
+      } else if (typeof exports['.'] === 'string') {
+        return dotExport;
+      }
+    } else if (!exports['./*']) {
+      throw new Error(`The root of the ${pkg.name} package is private`);
+    }
+  }
+
+  return isRequire ? pkg.main : pkg.module;
+}
+
 function getSourcePath(source, state, isRequire) {
-  const { resolveTo = isRequire ? pkg.main : pkg.exports['.'] || pkg.module } = state.opts;
+  const { resolveTo = getImportPathFromPkg(pkg, isRequire) } = state.opts;
   const match = pkgNameExp.exec(source);
   if (match && match[1] !== null && match[1] === pkg.name) {
     return normalize(join(relative(dirname(state.filename), root), resolveTo, match[2]));
